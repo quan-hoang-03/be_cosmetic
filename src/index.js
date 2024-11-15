b2a8869f3de56d3769425cda6bf3c446d8bd10b3
Quân Hoàng <quanhoang789a@gmail.com>
2024-11-16 01:38:09 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config()
+const app = express();
+const port = process.env.PORT || 3001
+
+app.get('/',(req,res)=>{
+    res.send('Hello world everyone')
+})
+
+app.listen(port, ()=>{
+    console.log('Server is running in port: ',+port)
+})   
