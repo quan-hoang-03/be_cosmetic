43590d0c1987fb5f107f0c0405bdd0f03a8ed04a
Quân Hoàng <quanhoang789a@gmail.com>
2024-11-16 01:48:26 +0700

    Update database and connect mongodb

--- src/models/OrderProduct.js
+++ src/models/OrderProduct.js
@@ -1,18 +1,41 @@
 const mongoose = require('mongoose')
-const productSchema = new mongoose.Schema(
+const orderSchema = new mongoose.Schema(
     {
-        name: {type: String, require: true, unique: true},
-        image: {type: String, require: true},
-        type: {type: String, require: true},
-        price: {type: Number, require: true},
-        countInStock: {type: Number, require: true},
-        rating: {type: Number, require: true},
-        description: {type: String, require: true},
+        orderItems: [
+            {
+                name: {type: String, require: true},
+                amount: {type: Number, require: true},
+                image: {type: String, require: true},
+                price: {type: Number, require: true},
+                product:{
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref:'Product',
+                    require:true
+                }
+            }
+        ],
+        shippingAddress:{
+            fullName: {type: String, require: true},
+            address: {type: String, require: true},
+            city: {type: String, require: true},
+            country: {type: String, require: true},
+            phone: {type: Number, require: true},
+        },
+        paymentMethod: {type: String, require: true},
+        itemsPrice: {type: Number, require: true},
+        shippingPrice: {type: Number, require: true},
+        taxPrice: {type: Number, require: true},
+        totalPrice: {type: Number, require: true},
+        user: { type:mongoose.Schema.Types.ObjectId,ref:'User',require:true},
+        isPaid: {type:Boolean,default:false},
+        paidAt: {type:Date},
+        isDelivered: {type:Boolean,default: false},
+        deliveredAt: {type:Date},
     },
     {
         // cập nhật thời gian tạo và update
         timestamps: true
     }
 )
-const Product = mongoose.model("Product", productSchema)
-module.exports = Product;
+const Order = mongoose.model("Order", orderSchema)
+module.exports = Order;
