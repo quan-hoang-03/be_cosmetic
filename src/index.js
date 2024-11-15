5048313bf8e1d9d8d6a52ebb82519ffd9d0139f6
Quân Hoàng <quanhoang789a@gmail.com>
2024-11-16 02:29:20 +0700

    config be

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config()
 const app = express();
 const port = process.env.PORT || 3001
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Hello world everyone')
 })
+mongoose.connect(`mongodb+srv://quanhoangcnpm1:0988935298@clustercosmetic.gyug2.mongodb.net/`).then(()=>{
+    console.log('Connect DB success');
+})
+.catch((err)=>{
+    console.log(err)
+})
 
-app.listen(port, ()=>{
-    console.log('Server is running in port: ',+port)
+app.listen(port, () => {
+    console.log('Server is running in port: ', +port)
 })   
