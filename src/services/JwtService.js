500094d6378347e89973c0f5e7186cf1538c490a
Quân Hoàng <quanhoang789a@gmail.com>
2024-11-19 01:03:45 +0700

    Tạo API đăng nhập kết hợp jsonwebtoken + refresh token

---
+++ src/services/JwtService.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+const generalAccessToken = async (payload) =>{
+    console.log('payload',payload)
+    const access_token = jwt.sign({
+        payload 
+    },'access_token',{expiresIn:'1h'})
+// expiresIn thời gian token hết hạn
+    return access_token
+}
+const generalRefreshAccessToken = async (payload) =>{
+    console.log('payload',payload)
+    const access_token = jwt.sign({
+        payload 
+    },'refresh_token',{expiresIn:'365d'})
+// expiresIn thời gian token hết hạn
+    return access_token
+}
+
+module.exports = {
+    generalAccessToken,
+    generalRefreshAccessToken
+}
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,4 +1,6 @@
 const User = require("../models/UserModel")
+const bcrypt = require("bcrypt")
+const { generalAccessToken, generalRefreshAccessToken } = require("./JWTService")
 
 const createUser = (newUser)=>{
     return new Promise( async (resolve,reject)=>{
@@ -13,11 +15,13 @@ const createUser = (newUser)=>{
                     message:'The email is already'
                 })
             }
+            //hash password mã hóa mật khẩu
+            const hash = bcrypt.hashSync(password,10)
             const createdUser = await User.create({
                 name,
                 email,
-                password,
-                confirmPassword,
+                password : hash,
+                confirmPassword : hash,
                 phone
             })
             if(createdUser){
@@ -32,6 +36,47 @@ const createUser = (newUser)=>{
         }
     })
 }
+const loginUser = (userLogin)=>{
+    return new Promise( async (resolve,reject)=>{
+        const {name,email,password,confirmPassword,phone} = userLogin
+        try{
+            const checkUser = await User.findOne({
+                email: email
+            })
+            if(checkUser === null){
+                resolve({
+                    status:'Ok',
+                    message:'Email này chưa được đăng ký'
+                })
+            }
+            const comparePassword = bcrypt.compareSync(password,checkUser.password)
+            if(!comparePassword){
+                resolve({
+                    status: 'Ok',
+                    message:'Mật khẩu hoặc tên đăng nhập không đúng',
+                })
+            }
+            const access_token = await generalAccessToken({
+                id: checkUser.id,
+                isAdmin: checkUser.isAdmin
+            })
+            const refresh_token = await generalRefreshAccessToken({
+                ud: checkUser.id,
+                isAdmin: checkUser.isAdmin
+            })
+            console.log('access_token',access_token)
+            resolve({
+                status: 'Ok',
+                message:'Success',
+                access_token,
+                refresh_token
+            })
+        }catch(e){
+            reject(e);
+        }
+    })
+}
 module.exports ={
-    createUser
+    createUser,
+    loginUser
 }
