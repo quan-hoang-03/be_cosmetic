c32b3a850165a00d84a7f885187d5e3f711d90e8
Quân Hoàng <quanhoang789a@gmail.com>
2024-12-22 03:47:05 +0700

    Fix : tải ảnh lên

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const routes = require('./routes');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-dotenv.config()
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+dotenv.config();
 const app = express();
-const port = process.env.PORT || 3001
-const MONGODB_URL = "mongodb+srv://quanhoangcnpm1:0988935298@clustercosmetic.gyug2.mongodb.net/";
+const port = process.env.PORT || 3001;
+const MONGODB_URL =
+  "mongodb+srv://quanhoangcnpm1:0988935298@clustercosmetic.gyug2.mongodb.net/";
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(cookieParser())
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+
 
 routes(app);
 
-mongoose.connect(MONGODB_URL, {
+mongoose
+  .connect(MONGODB_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
-    console.log('Connect DB success');
+    console.log("Connect DB success");
   })
   .catch((error) => {
-    console.error('Connect DB error:', error);
+    console.error("Connect DB error:", error);
   });
 
-
 app.listen(port, () => {
-    console.log('Server is running in port: ', +port)
-})   
+  console.log("Server is running in port:", port);
+});
